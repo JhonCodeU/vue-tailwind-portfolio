812dd5baa34768e3a8a5ce595f9c7ff3bbe6ccd3
Jhon deiber arcila castano <57459718+JhonCodeU@users.noreply.github.com>
2022-07-25 14:26:10 +0000

    Fixed image with taildwind

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,7 +4,7 @@
         <!-- The close button -->
         <button @click="closeModal" class="fixed z-90 top-6 right-8 text-white text-5xl font-bold">&times;</button>
         <!-- A big image will be displayed here -->
-        <img :src="url" class="max-w-[1000px] max-h-[800px] object-cover" />
+        <img :src="url" class="object-fill md:w-10/12 sm:w-96" />
     </div>
 </template>
 <script>
